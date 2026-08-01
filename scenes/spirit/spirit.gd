96ee4b80adb59e88436739f33041a51675dddb97
NicoCodesCode <nicolasrmz08@gmail.com>
2026-08-01 12:50:44 -0500

    Reload scene if player entered enemy's vision

--- scenes/spirit/spirit.gd
+++ scenes/spirit/spirit.gd
@@ -33,3 +33,8 @@ func _physics_process(_delta: float) -> void:
 			_target_position = marker_b.global_position
 		else:
 			_target_position = marker_a.global_position
+
+
+func _on_vision_cone_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().reload_current_scene()
