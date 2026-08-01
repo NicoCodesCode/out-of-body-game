171e78d6b1214140d312a527d624841e51e3e82b
NicoCodesCode <nicolasrmz08@gmail.com>
2026-08-01 10:26:37 -0500

    Add stamina mechanic

---
+++ scenes/player/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+
+var MAX_PRESENCE := 100.0
+var _soul_presence := MAX_PRESENCE
+var _drain_rate := 5.0
+var _recharge_rate := 20.0
+var _is_in_safe_zone := false
+
+
+func _physics_process(delta: float) -> void:
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
+	
+	move_and_slide()
+	
+	if _is_in_safe_zone:
+		_soul_presence = clampf(_soul_presence + _recharge_rate * delta, 0.0, MAX_PRESENCE)
+	else:
+		_soul_presence = clampf(_soul_presence - _drain_rate * delta, 0.0, MAX_PRESENCE)
+	
+	print("Soul Presence: ", _soul_presence)
+
+
+func _on_safe_zone_body_entered(body: Node2D) -> void:
+	if body == self:
+		_is_in_safe_zone = true
+
+
+func _on_safe_zone_body_exited(body: Node2D) -> void:
+	if body == self:
+		_is_in_safe_zone = false
