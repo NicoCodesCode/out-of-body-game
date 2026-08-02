38da1a370ed62104dede81fc77732195961dd5ff
NicoCodesCode <nicolasrmz08@gmail.com>
2026-08-02 17:25:19 -0500

    Add dialogues when interacting with the mirrors

---
+++ globals/state.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+var has_seen_bathroom_mirror_dialogue := false
+var has_seen_entrance_mirror_dialogue := false
+var has_seen_full_length_mirror_dialogue := false
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,27 +1,59 @@
 extends CharacterBody2D
 
 
+@export var interaction_detector: Area2D
+
 const SPEED = 150.0
 
 var MAX_PRESENCE := 100.0
 var _soul_presence := MAX_PRESENCE
 var _drain_rate := 10.0
 var _recharge_rate := 30.0
+
+var is_in_house := true
 var is_in_safe_zone := false
+
+
+func _ready() -> void:
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact"):
+		var actionables: Array[Area2D] = interaction_detector.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
 
 
 func _physics_process(delta: float) -> void:
 	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED
-	
 	move_and_slide()
 	
-	#if is_in_safe_zone:
-		#_soul_presence = clampf(_soul_presence + _recharge_rate * delta, 0.0, MAX_PRESENCE)
-	#else:
-		#_soul_presence = clampf(_soul_presence - _drain_rate * delta, 0.0, MAX_PRESENCE)
-	#
-	#print("Soul Presence: ", _soul_presence)
+	_manage_soul_presence(delta)
+
+
+func _manage_soul_presence(delta: float) -> void:
+	if is_in_house:
+		return
+	
+	if is_in_safe_zone:
+		_soul_presence = clampf(_soul_presence + _recharge_rate * delta, 0.0, MAX_PRESENCE)
+	else:
+		_soul_presence = clampf(_soul_presence - _drain_rate * delta, 0.0, MAX_PRESENCE)
+	
+	print("Soul Presence: ", _soul_presence)
 	
 	if _soul_presence == 0:
 		get_tree().reload_current_scene()
+
+
+func _on_dialogue_started(_resource: DialogueResource) -> void:
+	velocity = Vector2.ZERO
+	set_physics_process(false)
+
+
+func _on_dialogue_ended(_resource: DialogueResource) -> void:
+	set_physics_process(true)
